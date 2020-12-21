c60d27cbed37b90020cd791cd148e4d8d8d67e20
Marvin Laibold <marvin.laibold@aoe.com>
2020-12-21 20:14:22 +0100

    Add fonts, colors, dimens and styles

--- app/src/main/java/de/hs_rm/recipe_me/MainActivity.kt
+++ app/src/main/java/de/hs_rm/recipe_me/MainActivity.kt
@@ -2,17 +2,20 @@ package de.hs_rm.recipe_me
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import de.hs_rm.recipe_me.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
         }
     }
 }
--- app/src/main/java/de/hs_rm/recipe_me/ui/main/MainFragment.kt
+++ app/src/main/java/de/hs_rm/recipe_me/ui/main/MainFragment.kt
@@ -2,11 +2,13 @@ package de.hs_rm.recipe_me.ui.main
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import de.hs_rm.recipe_me.R
+import de.hs_rm.recipe_me.databinding.ShowcaseFragmentBinding
 
 class MainFragment : Fragment() {
 
@@ -15,10 +17,20 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var binding: ShowcaseFragmentBinding
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.showcase_fragment,
+            container,
+            false
+        )
+
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
