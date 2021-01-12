a11f8a61996bf99bc0c6d2fe2aa32a8317a98c82
Marvin Laibold <marvin.laibold@aoe.com>
2021-01-12 17:44:47 +0100

    Add tests

--- app/src/androidTest/java/de/hs_rm/recipe_me/declaration/declarations.kt
+++ app/src/androidTest/java/de/hs_rm/recipe_me/declaration/declarations.kt
@@ -1,4 +1,35 @@
 package de.hs_rm.recipe_me.declaration
 
-class declarations {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
+
+// https://medium.com/androiddevelopers/unit-testing-livedata-and-other-common-observability-problems-bb477262eb04
+/* Copyright 2019 Google LLC.
+   SPDX-License-Identifier: Apache-2.0 */
+fun <T> LiveData<T>.getOrAwaitValue(
+    time: Long = 2,
+    timeUnit: TimeUnit = TimeUnit.SECONDS
+): T {
+    var data: T? = null
+    val latch = CountDownLatch(1)
+    val observer = object : Observer<T> {
+        override fun onChanged(o: T?) {
+            data = o
+            latch.countDown()
+            this@getOrAwaitValue.removeObserver(this)
+        }
+    }
+
+    this.observeForever(observer)
+
+    // Don't wait indefinitely if the LiveData is not set.
+    if (!latch.await(time, timeUnit)) {
+        throw TimeoutException("LiveData value was never set.")
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    return data as T
 }
--- app/src/androidTest/java/de/hs_rm/recipe_me/service/RecipeRepositoryTest.kt
+++ app/src/androidTest/java/de/hs_rm/recipe_me/service/RecipeRepositoryTest.kt
@@ -1,4 +1,63 @@
 package de.hs_rm.recipe_me.service
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import de.hs_rm.recipe_me.declaration.getOrAwaitValue
+import kotlinx.coroutines.*
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import javax.inject.Inject
+
+/**
+ * https://developer.android.com/training/dependency-injection/hilt-testing
+ */
+@RunWith(AndroidJUnit4::class)
+@HiltAndroidTest
 class RecipeRepositoryTest {
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @Inject
+    lateinit var repository: RecipeRepository
+
+    val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+
+    @Before
+    fun init() {
+        hiltRule.inject()
+    }
+
+    @Test
+    fun testInjection() {
+        assertNotNull(repository)
+    }
+
+    @Test
+    fun addRecipeWithRelationsSucceed() {
+        //TODO
+    }
+
+    @Test
+    fun deleteRecipeWithRelationsSucceed() {
+        var recipes = repository.getRecipes().getOrAwaitValue()
+        val sizeBefore = recipes.size
+
+        // https://proandroiddev.com/testing-kotlin-coroutines-d904738b846d
+        runBlocking { repository.deleteRecipeWithRelations(recipes[0]) }
+
+        recipes = repository.getRecipes().getOrAwaitValue()
+        val sizeAfter = recipes.size
+
+        assertEquals(sizeBefore, sizeAfter + 1)
+    }
 }
