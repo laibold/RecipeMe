46c0657cd3a7727025e286549f75858edc6f00f3
Marvin Laibold <marvin.laibold@aoe.com>
2021-01-21 16:05:53 +0100

    Add string values to dimens

--- app/src/main/java/de/hs_rm/recipe_me/ui/recipe/detail/CookingStepListAdapter.kt
+++ app/src/main/java/de/hs_rm/recipe_me/ui/recipe/detail/CookingStepListAdapter.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import androidx.databinding.DataBindingUtil
-import de.hs_rm.recipe_me.R
 import de.hs_rm.recipe_me.databinding.CookingStepListitemBinding
 import de.hs_rm.recipe_me.model.recipe.CookingStep
 
