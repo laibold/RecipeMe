2ee29f1de8fb5379cda8033e62999a28718afa04
Christina Becker <beck_chri@web.de>
2021-01-21 19:17:36 +0100

    Add timer function

--- app/src/main/java/de/hs_rm/recipe_me/ui/recipe/detail/CookingStepFragment.kt
+++ app/src/main/java/de/hs_rm/recipe_me/ui/recipe/detail/CookingStepFragment.kt
@@ -1,18 +1,23 @@
 package de.hs_rm.recipe_me.ui.recipe.detail
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.AlarmClock
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import dagger.hilt.android.AndroidEntryPoint
 import de.hs_rm.recipe_me.R
 import de.hs_rm.recipe_me.databinding.CookingStepFragmentBinding
+import de.hs_rm.recipe_me.declaration.ui.fragments.CookingStepCallbackAdapter
+import de.hs_rm.recipe_me.model.recipe.CookingStep
 
 @AndroidEntryPoint
-class CookingStepFragment : Fragment() {
+class CookingStepFragment : Fragment(), CookingStepCallbackAdapter {
 
     private val viewModel: RecipeDetailViewModel by activityViewModels()
     private lateinit var binding: CookingStepFragmentBinding
@@ -43,10 +48,34 @@ class CookingStepFragment : Fragment() {
         val adapter = CookingStepListAdapter(
             requireContext(),
             R.layout.cooking_step_listitem,
-            viewModel.recipe.value!!.cookingSteps
+            viewModel.recipe.value!!.cookingSteps,
+            this
         )
 
         binding.cookingStepListView.adapter = adapter
+    }
+
+    /**
+     * Start a timer
+     * @param seconds time in seconds
+     * @param message message displayed on timer and when ending
+     */
+    private fun startTimer(message: String, seconds: Int) {
+        val intent = Intent(AlarmClock.ACTION_SET_TIMER).apply {
+            putExtra(AlarmClock.EXTRA_MESSAGE, message)
+            putExtra(AlarmClock.EXTRA_LENGTH, seconds)
+            putExtra(AlarmClock.EXTRA_SKIP_UI, false)
+        }
+        if (context?.let { intent.resolveActivity(it.packageManager) } != null) {
+            requireContext().startActivity(intent)
+        }
+    }
+
+    /**
+     * On callback from CookingStepListAdapter start the timer
+     */
+    override fun onCallback(cookingStep: CookingStep) {
+        startTimer(cookingStep.text, cookingStep.getTimeInSeconds())
     }
 
 }
