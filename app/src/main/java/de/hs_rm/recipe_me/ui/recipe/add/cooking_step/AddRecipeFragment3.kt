e060c8a8b6292e9d2bc8d3925f06389aba6cf73e
Marvin Laibold <marvin.laibold@aoe.com>
2021-02-19 21:38:38 +0100

    Add class for header_button

--- app/src/main/java/de/hs_rm/recipe_me/ui/recipe/add/cooking_step/AddRecipeFragment3.kt
+++ app/src/main/java/de/hs_rm/recipe_me/ui/recipe/add/cooking_step/AddRecipeFragment3.kt
@@ -49,7 +49,6 @@ class AddRecipeFragment3 : Fragment(), EditCookingStepAdapter {
             binding.cookingStepListView.adapter = adapter
 
             adapter?.notifyDataSetChanged()
-            // Scroll to bottom //TODO evaluieren
             binding.cookingStepListView.post {
                 binding.cookingStepListView.setSelection(adapter!!.count - 1)
             }
