1cc387b6ae156275752423b89109cda3031b156c
Marvin Laibold <marvin.laibold@aoe.com>
2021-03-27 17:03:14 +0100

    WIP: Add Fragment and preferences

---
+++ app/src/main/java/de/hs_rm/recipe_me/ui/profile/SettingsFragment.kt
@@ -0,0 +1,12 @@
+package de.hs_rm.recipe_me.ui.profile
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        TODO("Not yet implemented")
+    }
+
+}
